bdcbb65efd46faa295c2ecbb908fa59578d11f14
Stuart Reid <stuartreid81@gmail.com>
2026-08-16 00:12:51 +0100

    refactor: rename EarthDefence API project to EarthDefence.Api

--- EarthDefence.Shared/obj/Debug/net10.0/EarthDefence.Shared.AssemblyInfo.cs
+++ EarthDefence.Shared/obj/Debug/net10.0/EarthDefence.Shared.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EarthDefence.Shared")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ae1251c8cb91aa855966bc838d507ab720972ea7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+50ae6b146b40e89dd112f5eebe9f6e93098388a5")]
 [assembly: System.Reflection.AssemblyProductAttribute("EarthDefence.Shared")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EarthDefence.Shared")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
