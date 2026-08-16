18dadcf3eb63d70de5faa59c7beda4daafc3add9
Stuart Reid <stuartreid81@gmail.com>
2026-08-17 00:06:47 +0100

    feat: configure dynamic HttpClient and integrate Tailwind CSS v4

--- EarthDefence.Blazor/Program.cs
+++ EarthDefence.Blazor/Program.cs
@@ -6,6 +6,8 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+var apiBaseAddress = builder.Configuration["ApiBaseAddress"] ?? builder.HostEnvironment.BaseAddress;
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(apiBaseAddress) });
 
 await builder.Build().RunAsync();
